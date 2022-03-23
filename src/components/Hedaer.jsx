02812fa0f2cd433a4a103add6f28a4d82243bd6d
SinilovNazar <102222176+SinilovNazar@users.noreply.github.com>
2022-03-23 15:41:22 +0300

    project

---
+++ src/components/Hedaer.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return <header className='header'>
+        <img className='header-logo' src="https://public-assets.envato-static.com/assets/logos/envato_market-a5ace93f8482e885ae008eb481b9451d379599dfed24868e52b6b2d66f5cf633.svg" />
+    </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+
+
+const Profile = () => {
+    return   <div className='content'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrTFrhr_-pYR74jUgOy7IerAoHAX3zPIZZcg&usqp=CAU' />
+
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New Post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>;
+}
+
+export default Profile;
